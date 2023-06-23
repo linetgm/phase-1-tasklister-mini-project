0759772f2a214aa13a683ea9c8e9d3724a6ff51b
Linet <muthiigathoni@gmail.com>
2023-06-23 09:45:15 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  let form = document.getElementById('createTaskForm')
+  let list = document.getElementById('list')
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let task = document.getElementById('newTaskDescription').value
+    let li = document.createElement('li')
+    li.innerText = task
+    let editButton = document.createElement('button')
+    editButton.innerText = 'Edit'
+editButton.addEventListener('click', (e) => {
+      let editTask = prompt('Edit Task')
+      li.innerText = editTask
+    })
+    li.appendChild(editButton)
+    let deleteBtn = document.createElement('button')
+    deleteBtn.innerText = 'x'
+    deleteBtn.addEventListener('click', (e) => {
+      e.target.parentElement.remove()
+    })
+li.appendChild(deleteBtn)
+
+
+    list.appendChild(li)
+    form.reset()
+  })
 });
